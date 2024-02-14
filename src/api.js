94622aaec527ff8b6a8bdbad76dcf34ca98fbf5c
jtrevino29 <jtrevino701@gmail.com>
2024-02-13 19:23:54 -0600

    debugging

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": 'a8a523a77bmsh288f0712af73db1p1aaaf6jsn06a247b6c417',// enter your rapid api key here
+      "X-RapidAPI-Host": 'forecast9.p.rapidapi.com'
+    },
+  };
+  export const GEO_API_URL = "https://forecast9.p.rapidapi.com/rapidapi/forecast/Berlin/summary/";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "bc1de7a94f6622a9d8601e4da6c93fbe"; // enter your key from openweather API
